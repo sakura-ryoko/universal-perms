08cebcabf47e5f0cfac1ce846f874c0933f12dd5
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2024-06-24 13:16:14 +0300

    Update to 1.21

--- src/main/java/io/github/mattidragon/universalperms/UniversalPerms.java
+++ src/main/java/io/github/mattidragon/universalperms/UniversalPerms.java
@@ -20,8 +20,9 @@ public class UniversalPerms implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.addPhaseOrdering(Event.DEFAULT_PHASE, new Identifier("universal_perms", "after"));
-        CommandRegistrationCallback.EVENT.register(new Identifier("universal_perms", "after"), (dispatcher, registryAccess, environment) -> {
+        var phaseId = Identifier.of("universal_perms", "after");
+        CommandRegistrationCallback.EVENT.addPhaseOrdering(Event.DEFAULT_PHASE, phaseId);
+        CommandRegistrationCallback.EVENT.register(phaseId, (dispatcher, registryAccess, environment) -> {
             alterNode(dispatcher.getRoot(), new ArrayDeque<>(), new HashMap<>());
             LOGGER.info("Applied cursed permissions!");
         });
